711d26f7eab562521a4454d13986a0f204e6faac
yaroslavberezin <yb2003work@gmail.com>
2021-11-15 20:02:50 +0300

    V1.07

--- src/ru/vsu/sc/berezin_y_a/Main.java
+++ src/ru/vsu/sc/berezin_y_a/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         TestProgram testProgram = new TestProgram();
+        Solution solution = new Solution();
 
         if (!testProgram.test()) {
             System.out.println("Test error");
@@ -21,7 +22,7 @@ public class Main {
         readArray(array);
 
         System.out.print("Answer: ");
-        System.out.println(findCountOfSatisfyingElements(array));
+        System.out.println(solution.findCountOfSatisfyingElements(array));
 
     }
 
@@ -37,29 +38,6 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("Enter %d element of array: ", (i + 1));
             arr[i] = scanner.nextInt();
-        }
-    }
-
-    public static int findCountOfSatisfyingElements(int[] arr) {
-        int sum = 0;
-        for (int element : arr) {
-            sum += element;
-        }
-        double averageArithmetic = (double) sum / arr.length;
-        int countOfNumbers = 0;
-        double minDifference = 99999;
-        if (arr.length == 0) {
-            return 0;
-        } else {
-            for (double element : arr) {
-                if (Math.abs(element - averageArithmetic) < minDifference) {
-                    minDifference = Math.abs(element - averageArithmetic);
-                    countOfNumbers = 1;
-                } else if (Math.abs(element - averageArithmetic) == minDifference) {
-                    countOfNumbers++;
-                }
-            }
-            return countOfNumbers;
         }
     }
 
--- src/ru/vsu/sc/berezin_y_a/TestProgram.java
+++ src/ru/vsu/sc/berezin_y_a/TestProgram.java
@@ -3,36 +3,37 @@ package ru.vsu.sc.berezin_y_a;
 public class TestProgram {
 
     TestCase testCase = new TestCase();
+    Solution solution = new Solution();
 
     public boolean test() {
-        if (Main.findCountOfSatisfyingElements(testCase.arr1) != 2) {
+        if (solution.findCountOfSatisfyingElements(testCase.arr1) != 2) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr2) != 2) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr2) != 2) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr3) != 2) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr3) != 2) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr4) != 3) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr4) != 3) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr5) != 4) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr5) != 4) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr6) != 2) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr6) != 2) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr7) != 1) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr7) != 1) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr8) != 1) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr8) != 1) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr9) != 1) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr9) != 1) {
             System.out.println("Test 1 is not completed");
             return false;
-        } else if (Main.findCountOfSatisfyingElements(testCase.arr10) != 2) {
+        } else if (solution.findCountOfSatisfyingElements(testCase.arr10) != 2) {
             System.out.println("Test 1 is not completed");
             return false;
         } else {
